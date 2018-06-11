3b92ea584b6faa5fd212ef34e4bf59259a60584d
Kevin Lorenzo <kevincoshic25@gmail.com>
2018-06-11 11:52:45 -0600

    Views for Alumno, Profesor, Grado and AlumnoGrado Added

---
+++ ViaroNetworksApp/ViaroNetworksApp/Controllers/AlumnoHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViaroNetworksApp.Controllers
+{
+    public class AlumnoHomeController : Controller
+    {
+        // GET: AlumnoHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViaroNetworksApp/ViaroNetworksApp/Controllers/ProfesorHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViaroNetworksApp.Controllers
+{
+    public class ProfesorHomeController : Controller
+    {
+        // GET: ProfesorHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
