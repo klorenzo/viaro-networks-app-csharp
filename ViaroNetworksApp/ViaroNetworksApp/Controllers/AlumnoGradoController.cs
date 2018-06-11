12a877c42a40d7868d5c85e35b621535419246d8
Kevin Lorenzo <kevincoshic25@gmail.com>
2018-06-11 10:58:58 -0600

    AlumnoGradoController Added

---
+++ ViaroNetworksApp/ViaroNetworksApp/Controllers/AlumnoGradoController.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ViaroNetworksApp.Data;
+using ViaroNetworksApp.Models;
+
+namespace ViaroNetworksApp.Controllers
+{
+    public class AlumnoGradoController : ApiController
+    {
+        // GET: api/alumnogrado
+        public IEnumerable<AlumnoGrado> Get()
+        {
+            return AlumnoGradoRepository.GetAll();
+        }
+
+        // GET: api/alumnogrado/1
+        public AlumnoGrado Get(int id)
+        {
+            return AlumnoGradoRepository.GetByID(id);
+        }
+
+        // POST: api/alumnogrado
+        public JObject Post([FromBody]AlumnoGrado alumnoGrado)
+        {
+            JObject response = new JObject
+            {
+                { "error", false },
+                { "msg", "AlumnoGrado created successfully." }
+            };
+
+            if (!AlumnoGradoRepository.Create(alumnoGrado))
+            {
+                response["error"] = true;
+                response["msg"] = "AlumnoGrado not created.";
+            }
+
+            return response;
+        }
+
+        // PUT: api/alumnogrado/5
+        public JObject Put(int id, [FromBody]AlumnoGrado alumnoGrado)
+        {
+            alumnoGrado.ID = id;
+
+            JObject response = new JObject
+            {
+                { "error", false },
+                { "msg", "AlumnoGrado updated successfully." }
+            };
+
+            if(!AlumnoGradoRepository.Update(alumnoGrado))
+            {
+                response["error"] = true;
+                response["msg"] = "AlumnoGrado not updated.";
+            }
+            return response;
+        }
+
+        // DELETE: api/alumnogrado/1
+        public JObject Delete(int id)
+        {
+            JObject response = new JObject
+            {
+                { "error", false },
+                { "msg", "AlumnoGrado deleted successfully." }
+            };
+
+            if(!AlumnoGradoRepository.Delete(id))
+            {
+                response["error"] = true;
+                response["msg"] = "AlumnoGrado not deleted.";
+            }
+
+            return response;
+        }
+    }
+}
